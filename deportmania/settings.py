f3b971ff078c8a7409663ed95e779f321ab08fb0
Alejandro <alejandro@alejandro-SATELLITE-L755.(none)>
2015-08-03 18:04:09 +0200

    configurado para desplegar en heroku

--- deportmania/settings.py
+++ deportmania/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = '1m8g5&hjlsfi@obef1d4a6&bu)x1+o-0s__h4ct*dp#mm+8^*1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -98,19 +98,24 @@ SOUTH_DATABASE_ADAPTERS = {
     'default': 'south.db.mysql'
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'deportmania',  # Or path to database file if using sqlite3.
-        'USER': 'root',  # Not used with sqlite3.
-        'PASSWORD': '',  # Not used with sqlite3.
-        'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',  # Set to empty string for default. Not used with sqlite3.
-        'TEST_NAME': 'deportmania',
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.mysql',
+   #     'NAME': 'deportmania',  # Or path to database file if using sqlite3.
+    #    'USER': 'root',  # Not used with sqlite3.
+     #   'PASSWORD': '',  # Not used with sqlite3.
+      #  'HOST': '',  # Set to empty string for localhost. Not used with sqlite3.
+       # 'PORT': '',  # Set to empty string for default. Not used with sqlite3.
+        #'TEST_NAME': 'deportmania',
 
-    },
+    #},
 
-}
+#}
+
+import dj_database_url
+DATABASES  =  dj_database_url.config()
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
@@ -136,8 +141,7 @@ MEDIA_URL = 'http://localhost:8000/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-# URL prefix for admin static files -- CSS, JavaScript and images.
+STATIC_ROOT = 'staticfiles'# URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
 ADMIN_MEDIA_PREFIX = '/static/admin/'
